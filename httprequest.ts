ea5c36c8619658fe6734d21e68aeb3a144b77a8c
dwaynissh <dwaynissh@gmail.com>
2023-09-13 00:48:07 +0100

    Httprequest

---
+++ httprequest.ts
@@ -0,0 +1,48 @@
+import http, { IncomingMessage, ServerResponse } from "http";
+
+import fs from "fs"
+import path from "path"
+
+const port: number = 4000;
+
+
+const server = http.createServer (
+    (req: IncomingMessage, res: ServerResponse<IncomingMessage>)=> {
+        res.writeHead(200);
+
+        let connect: string = "./site/"
+        switch (req.url) {
+            case "/":
+                connect += "home.html"
+                res.statusCode = 200;
+                break;
+            case "/about": 
+                connect += "about/html";
+                res.statusCode = 200;
+                break;
+            case "/contact":
+                connect += "contact.html";
+                res.statusCode = 200;
+                break;
+            default:
+                connect += "404.html";
+                res.statusCode =  404;
+                break
+        }
+
+        fs.readFile(path.join(__dirname, connect), (error, data)=>{
+            if (error) {
+                console.log("An error occured", error);
+                res.end();
+            } else {
+                res.write(data);
+                res.end()
+            }
+        });
+    }
+);
+
+server.listen (port, () => {
+    console.log("");
+    console.log("server is listening to port on port", port);
+})
